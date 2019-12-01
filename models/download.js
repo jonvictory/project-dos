66a5ed7b271606f15eb574563345e53d91a132ca
Tecdef <37391025+Tecdef@users.noreply.github.com>
2019-12-01 11:30:50 -0700

    Update download.js

--- models/download.js
+++ models/download.js
@@ -5,8 +5,8 @@ const pkgcloud = require('pkgcloud');
 
 var client = pkgcloud.storage.createClient({
     provider: 'amazon',
-    keyId: 'AKIAJYBJDMHXDTUROUHQ', // access key id
-    key: 'j0OhIoMpDyz0AJ3Zc9w//3SHIXpqOjYVUQHtGkpT', // secret key
+    keyId: key, // access key id
+    key: key, // secret key
     region: 'us-west-2', // region
   });
 
