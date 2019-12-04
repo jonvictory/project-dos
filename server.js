90d7dbed5624238b2f4eda4b65e5074f16241d65
Steven Hofheins <stevenisinsane@gmail.com>
2019-12-03 20:28:36 -0700

    adding integrating .gitignore pkgcloud aws

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var passport = require("./config/passport");
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
+var storage = require("./Storage/download")
+const dotenv = require('dotenv');
+var pkgcloud = require('pkgcloud');
+var fs = require('fs');
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
